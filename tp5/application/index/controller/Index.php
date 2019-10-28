0b3e74f330594a2063cfa707ccae60b83ab6e23d
kailunli <947130141@qq.com>
2019-10-28 16:52:36 +0800

    add

--- tp5/application/index/controller/Index.php
+++ tp5/application/index/controller/Index.php
@@ -11,18 +11,10 @@ class Index extends Base
     {
         $pageNum = $req->param('pageNum', 1, 'int');
         $pageSize = $req->param('pageSize', 20, 'int');
-<<<<<<< HEAD
-
         $cserv = new ClassicService();
         $result = $cserv->getClassicList($pageNum, $pageSize);
 
         return json($result);
-=======
-
-        $cserv = new ClassicService();
-        $result = $cserv->getClassicList($pageNum, $pageSize);
-
->>>>>>> 0a9d9483109e8d8931613cb254da841e7f2bc400
     }
 
 }
