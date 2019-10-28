46f4e5da6d4c7048cc817b787de87ca8a02be82c
kailunli <947130141@qq.com>
2019-10-28 16:47:47 +0800

    add

---
+++ tp5/application/common/controller/Base.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\common\controller;
+
+use think\Controller;
+
+class Base extends Controller
+{
+    protected function initialize()
+    {
+        parent::initialize();
+    }
+}
